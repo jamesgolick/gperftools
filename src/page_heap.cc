300b2445e20858c6baf4f574a2110245b6e4608a
James Golick <jamesgolick@gmail.com>
2013-10-30 17:20:28 +0100

    handle the case where best is null

--- src/page_heap.cc
+++ src/page_heap.cc
@@ -197,7 +197,7 @@ Span* PageHeap::AllocLarge(Length n) {
     best = large_skiplist_.GetBestFit(n);
   }
 
-  if (best->location == Span::ON_NORMAL_FREELIST) {
+  if (best == NULL || best->location == Span::ON_NORMAL_FREELIST) {
     return best == NULL ? NULL : Carve(best, n);
   }
 
